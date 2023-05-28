05a5988b37775a288c204e1230108b229bb220b2
joaoJs <j.campos4g@gmail.com>
2023-05-28 09:32:04 -0400

    adds dummy route:

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom' 
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
 
 function App() {
-    return <h1>Hello</h1>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing}></Route>
+                    <Route exact path="/surveys" component={Dashboard}></Route>
+                    <Route path="/surveys/new" component={SurveyNew}></Route>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
