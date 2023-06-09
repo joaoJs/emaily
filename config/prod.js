1f1078a19b96e7ca5196af0e998b38e6ffe5a611
joaoJs <j.campos4g@gmail.com>
2023-06-09 09:07:11 -0400

    adds sendgrid boilerplate code and Mailer class

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,7 @@ module.exports = {
     mongoURI: process.env.mongoURI,
     cookieKey: process.env.cookieKey,
     stripePublishableKey: process.env.stripePublishableKey,
-    stripeSecretKey: process.env.stripeSecretKey
+    stripeSecretKey: process.env.stripeSecretKey,
+    sendGridKey: process.env.sendGridKey,
+    fromEmail: process.env.fromEmail
 }
